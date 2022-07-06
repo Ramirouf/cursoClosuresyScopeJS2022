e2fe2957e58aad3cb65b1dbb59fb8768b45d7719
Ramiro Uffelmann <ramirouf@gmail.com>
2022-07-05 23:31:06 -0300

    Added leftovers

--- src/scope/global.js
+++ src/scope/global.js
@@ -1,3 +1,16 @@
 // Global Scope
 
 var fruit = "Apple"; //global
+function bestFruit(){
+    console.log(fruit);
+}
+
+bestFruit();
+
+function countries(){
+    country = "India";
+    console.log(country);
+}
+
+countries();
+console.log(country);
