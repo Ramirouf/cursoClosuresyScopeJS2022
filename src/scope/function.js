1f672cc193a6c33d04fe29618b45ff41ad0510c7
Ramiro Uffelmann <ramirouf@gmail.com>
2022-07-06 08:36:39 -0300

    Finished 3rd class (function scope)

---
+++ src/scope/function.js
@@ -0,0 +1,11 @@
+function greeting() {
+    let name = 'John';
+    console.log(name);
+
+    if (name === 'John') {
+        console.log(`Hello ${name}`);
+    }
+}
+
+greeting();
+//console.log(name);
